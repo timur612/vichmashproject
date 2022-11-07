401b0b0d0e9a2ad5ddb3b1bad3017e59f7d2274d
timur612 <tz220404@gmail.com>
2022-11-07 17:03:41 +0300

    deployment

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+cd -
